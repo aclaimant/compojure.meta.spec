8d0cd601d28f50b0252d012284d3ef1854304d60
Joel Friedman and Mark Nicolosi <mark@aclaimant.com>
2016-07-14 15:00:42 -0500

    Middleware for enforces specs on requests and responses

--- src/aclaimant/compojure/meta/spec.clj
+++ src/aclaimant/compojure/meta/spec.clj
@@ -1,6 +1,27 @@
-(ns aclaimant.compojure.meta.spec)
+(ns aclaimant.compojure.meta.spec
+  (:require
+    [clojure.spec :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn ^:private assert-conforms [spec v]
+  (s/assert spec v)
+  (s/conform spec v))
+
+(defn wrap-request-spec
+  "Middleware to use :request-spec route metadata to enforce a spec on (:params request)"
+  [handler]
+  (fn [{:keys [params metadata] :as req}]
+    (handler
+      (if-let [request-spec (:request-spec metadata)]
+        (assoc req
+               :params (assert-conforms request-spec params)
+               :unconformed-params params)
+        req))))
+
+(defn wrap-response-spec
+  "Middleware to use :response-spec route metadata to enforce a spec on route response"
+  [handler]
+  (fn [{:keys [params metadata] :as req}]
+    (let [response (handler req)]
+      (if-let [response-spec (:response-spec metadata)]
+        (assert-conforms response-spec response)
+        response))))
--- test/aclaimant/compojure/meta/spec_test.clj
+++
@@ -1,7 +0,0 @@
-(ns aclaimant.compojure.meta.spec-test
-  (:require [clojure.test :refer :all]
-            [aclaimant.compojure.meta.spec :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
