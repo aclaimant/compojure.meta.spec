e31ddc336094c9e48499e0c0450f1b8065893866
Joel Friedman and Mark Nicolosi <mark@aclaimant.com>
2016-07-14 14:55:27 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to compojure.meta.spec
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/aclaimant/compojure/meta/spec_test.clj
@@ -0,0 +1,7 @@
+(ns aclaimant.compojure.meta.spec-test
+  (:require [clojure.test :refer :all]
+            [aclaimant.compojure.meta.spec :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
