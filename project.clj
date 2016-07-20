471549c8712e8dd12adb50788ee865e9fedea00a
Joel Friedman and Mark Nicolosi <mark@aclaimant.com>
2016-07-20 10:01:03 -0500

    0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.aclaimant/compojure.meta.spec "0.1.2"
+(defproject com.aclaimant/compojure.meta.spec "0.1.3-SNAPSHOT"
   :description "Enforce clojure.specs on compojure requests and responses"
   :url "https://aclaimant.com/"
   :license {:name "Eclipse Public License"
