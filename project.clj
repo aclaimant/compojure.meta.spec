8b4608e01936c15f307384a1f21efa418822ade5
Case Nelson and Mark Nicolosi <mark@aclaimant.com>
2017-06-21 12:39:13 -0500

    Bumps compojure.meta

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [org.clojure/test.check "0.9.0"]
-                 [com.aclaimant/compojure.meta "0.1.0"]])
+                 [com.aclaimant/compojure.meta "0.3.1"]])
